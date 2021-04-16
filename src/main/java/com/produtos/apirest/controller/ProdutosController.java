d32c01deec1aa54f53f1570ce89f0f065b403407
Mário André <mario_andre.l.a@hotmail.com>
2021-04-16 15:21:00 -0300

    definido arquivo config Swagger

--- src/main/java/com/produtos/apirest/controller/ProdutosController.java
+++ src/main/java/com/produtos/apirest/controller/ProdutosController.java
@@ -2,6 +2,8 @@ package com.produtos.apirest.controller;
 
 import com.produtos.apirest.models.Produto;
 import com.produtos.apirest.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,31 +11,39 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value = "API REST Produtos") //Titulo da API
+@CrossOrigin(origins = "*") // Controla quem pode acessar a API. Com "*" qualquer dominio pode acessar a API. Para especificar um dominio fazer "/http:dominio"
 public class ProdutosController {
+
     @Autowired
     ProdutoRepository produtoRepository;
 
     @GetMapping("/produtos")
+    @ApiOperation(value = "Retorna uma lista de produtos")
     public List<Produto> listaProdutos(){
         return produtoRepository.findAll();
     }
 
     @GetMapping("/produtos/{id}")
+    @ApiOperation(value = "Retorna um produto unico")
     public Produto listaProdutoUnico(@PathVariable(value = "id") long id){
         return produtoRepository.findById(id);
     }
 
     @PostMapping("/produto")
+    @ApiOperation(value = "Salva um produto")
     public Produto salvaProduto(@RequestBody Produto produto){
         return produtoRepository.save(produto);
     }
 
     @DeleteMapping("/produto")
+    @ApiOperation(value = "Deleta um produto")
     public void deletaProduto(@RequestBody Produto produto){
         produtoRepository.delete(produto);
     }
 
     @PutMapping("/produto")
+    @ApiOperation(value = "Atualiza um produto")
     public Produto atualizaProduto(@RequestBody Produto produto){
         return produtoRepository.save(produto);
     }
